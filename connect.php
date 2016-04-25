59f6d3cb771adb42563da3bb900c149a9cf479d1
cvxvdugarte <cvx.vdugarte@gmail.com>
2016-04-25 14:23:16 -0430

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 $user = $_POST["inputUser"]; 
 $pass = $_POST["inputPassword"]; 
-$mysqli = mysqli_connect("	mysql.hostinger.es", "u754135709_vddb", "H4NZ0h4tt0r1", "u754135709_dbvd");
+$mysqli = mysqli_connect("mysql.hostinger.es", "u754135709_vddb", "H4NZ0h4tt0r1", "u754135709_dbvd");
 if (mysqli_connect_errno($mysqli)) {
     echo "Fallo al conectar a MySQL: " . mysqli_connect_error();
 }
@@ -11,12 +11,9 @@ FROM  `USUARIOS` where codigo = ".$user);
 $fila = mysqli_fetch_assoc($resultado);
 echo $fila['_msg'];
 
-$mysqli = new mysqli("ejemplo.com", "usuario", "contraseña", "basedatos");
+$mysqli = new mysqli("mysql.hostinger.es", "u754135709_vddb", "H4NZ0h4tt0r1", "u754135709_dbvd");
 if ($mysqli->connect_errno) {
     echo "Fallo al conectar a MySQL: " . $mysqli->connect_error;
 }
 
-$resultado = $mysqli->query("SELECT 'elecciones para complacer a todos.' AS _msg FROM DUAL");
-$fila = $resultado->fetch_assoc();
-echo $fila['_msg'];
 ?>
