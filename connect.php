d22b4029e52bf38f0a915d9cb2decbe43499d7fe
cvxvdugarte <cvx.vdugarte@gmail.com>
2016-04-25 14:40:14 -0430

    Update connect.php

--- connect.php
+++ connect.php
@@ -45,7 +45,31 @@ if ($mysqli->connect_errno) {
 
     <div class="wrapper">
       <section>
-    
+    <table>
+<tr>
+  <td><strong>Curso</strong></td>
+  <td><strong>Horas</strong></td>
+  <td><strong>Horario</strong></td>
+</tr>
+ 
+<tr>
+  <td>CSS</td>
+  <td>20</td>
+  <td>16:00 - 20:00</td>
+</tr>
+ 
+<tr>
+  <td>HTML</td>
+  <td>20</td>
+  <td>16:00 - 20:00</td>
+</tr>
+ 
+<tr>
+  <td>Dreamweaver</td>
+  <td>60</td>
+  <td>16:00 - 20:00</td>
+</tr>
+</table>
       </section>
   
   
