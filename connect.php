56bdeb3e2ec5face0e08d13433d1556ac4c464d8
cvxvdugarte <cvx.vdugarte@gmail.com>
2016-04-25 14:14:48 -0430

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,10 +1,13 @@
 <?php
+$user = $_POST["inputUser"]; 
+$pass = $_POST["inputPassword"]; 
 $mysqli = mysqli_connect("	mysql.hostinger.es", "u754135709_vddb", "H4NZ0h4tt0r1", "u754135709_dbvd");
 if (mysqli_connect_errno($mysqli)) {
     echo "Fallo al conectar a MySQL: " . mysqli_connect_error();
 }
 
-$resultado = mysqli_query($mysqli, "SELECT 'Un mundo lleno de ' AS _msg FROM DUAL");
+$resultado = mysqli_query($mysqli, "SELECT * 
+FROM  `USUARIOS` where codigo = ".$user);
 $fila = mysqli_fetch_assoc($resultado);
 echo $fila['_msg'];
 
