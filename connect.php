97e6abe718d6edb298f2dbe928d9cad6c11286ee
cvxvdugarte <cvx.vdugarte@gmail.com>
2016-04-25 14:37:35 -0430

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,22 +1,3 @@
-<?php
-$user = $_POST["inputUser"]; 
-$pass = $_POST["inputPassword"]; 
-$mysqli = mysqli_connect("mysql.hostinger.es", "u754135709_vddb", "H4NZ0h4tt0r1", "u754135709_dbvd");
-if (mysqli_connect_errno($mysqli)) {
-    echo "Fallo al conectar a MySQL: " . mysqli_connect_error();
-}
-
-$resultado = mysqli_query($mysqli, "SELECT * 
-FROM  `USUARIOS` where codigo = ".$user);
-$fila = mysqli_fetch_assoc($resultado);
-echo $fila['_msg'];
-
-$mysqli = new mysqli("mysql.hostinger.es", "u754135709_vddb", "H4NZ0h4tt0r1", "u754135709_dbvd");
-if ($mysqli->connect_errno) {
-    echo "Fallo al conectar a MySQL: " . $mysqli->connect_error;
-}
-
-?>
 <!doctype html>
 <html>
   <head>
@@ -40,7 +21,7 @@ if ($mysqli->connect_errno) {
       <?php
 $user = $_POST["inputUser"]; 
 $pass = $_POST["inputPassword"]; 
-$mysqli = mysqli_connect("mysql.hostinger.es", "u754135709_vddb", "H4NZ0h4tt0r1", "u754135709_dbvd");
+$mysqli = mysqli_connect("mysql.hoxstinger.es", "u754135709_vddb", "H4NZ0h4tt0r1", "u754135709_dbvd");
 if (mysqli_connect_errno($mysqli)) {
     echo "Fallo al conectar a MySQL: " . mysqli_connect_error();
 }
@@ -50,10 +31,9 @@ $fila = mysqli_fetch_assoc($resultado);
 echo $fila['_msg'];
 $mysqli = new mysqli("mysql.hostinger.es", "u754135709_vddb", "H4NZ0h4tt0r1", "u754135709_dbvd");
 if ($mysqli->connect_errno) {
-    echo "Fallo al conectar a MySQL: " . $mysqli->connect_error;
+   echo "Fallo al conectar a MySQL: " . $mysqli->connect_error;
 } 
-else
-{echo "good job";}
+
 ?>
   </head>
   <body>
