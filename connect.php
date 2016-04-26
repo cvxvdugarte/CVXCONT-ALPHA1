9a5b62f2588784cad382975666be78ff0e9256ef
cvxvdugarte <cvx.vdugarte@gmail.com>
2016-04-26 08:38:59 -0430

    Update connect.php

--- connect.php
+++ connect.php
@@ -21,21 +21,25 @@
       <?php
 $user = $_POST["inputUser"]; 
 $pass = $_POST["inputPassword"]; 
+
 $mysqli = mysqli_connect("mysql.hostinger.es", "u754135709_vddb", "H4NZ0h4tt0r1", "u754135709_dbvd");
 if (mysqli_connect_errno($mysqli)) {
     echo "Fallo al conectar a MySQL: " . mysqli_connect_error();
 }
-$resultado = mysqli_query($mysqli, "SELECT codigo 
-FROM  USUARIOS where codigo = ".$user);
 
 $mysqli = new mysqli("mysql.hostinger.es", "u754135709_vddb", "H4NZ0h4tt0r1", "u754135709_dbvd");
 if ($mysqli->connect_errno) {
    echo "Fallo al conectar a MySQL: " . $mysqli->connect_error;
 } 
+$resultado = mysqli_query($mysqli, "SELECT codigo 
+FROM  USUARIOS");
 
 ?>
   </head>
   <body>
+    <?php
+    echo $user.$pass.$resultado;
+    ?>
       <div id="header">
         <svg height="30" width="500">
         <text x="15" y="15" fill="gray">App administrativa y contable</text>
