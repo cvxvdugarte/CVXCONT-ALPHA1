2a93d10810e5775f51c8eb9bc956b262bbe17314
V.Dugarte <cvxvdugarte@users.noreply.github.com>
2016-06-01 16:44:46 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -16,7 +16,7 @@
   <body>
   <?php
   $cod = $_POST["codigo"];
-$mysqli = new mysqli("mysql.hostinger.es", "u754135709_vddb", "", "u754135709_dbvd");
+$mysqli = new mysqli("");
 if ($mysqli->connect_errno) {
    echo "Fallo al conectar a MySQL: " . $mysqli->connect_error;
                             }
