14dbd8a58d41e5d0c95502b4b8ea6a2fd07ad89e
V.Dugarte <cvxvdugarte@users.noreply.github.com>
2016-06-03 11:37:41 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -20,8 +20,7 @@ $mysqli = new mysqli("mysql.hostinger.es","u754135709_vddb","*#L4S3PT1M4D3LM4DR1
 if ($mysqli->connect_errno) {
    echo "Fallo al conectar a MySQL: " . $mysqli->connect_error;
                             }
-$resultado = $mysqli->query('SELECT  `CODIGO` FROM `USUARIOS` WHERE `CODIGO` = $_POST["codigo"]'); 
-
+$resultado = $mysqli->query('SELECT  CODIGO FROM USUARIOS WHERE CODIGO = $cod'); 
 echo $resultado;
 $mysqli->close();
 /*$nombreConexion = mysqli_connect("mysql.hostinger.es" ,"u754135709_vddb" , "*#L4S3PT1M4D3LM4DR1D","u754135709_dbvd");
