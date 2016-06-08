bade3f549ee1135e0c62cb79a6c574148c8b18c2
cvxcont <cvxvdugarte@users.noreply.github.com>
2016-06-08 14:22:58 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -27,7 +27,10 @@ echo $consulta."<br><br><br><br><br><br>";
 echo $cod.$key."<br><br><br><br><br><br>";
 echo "Connected successfully"; 
 $resultado = $mysqli->query($consulta);
-$num_resultados=$re->num_rows;
+if (!$resultado) {
+    throw new Exception("Database Error [{$mysqli->errno}] {$mysqli->error}");
+}
+$num_resultados=$resultado->num_rows;
 
     //$resultado->data_seek($num_fila);
     $REGISTRO = $resultado->fetch_assoc();
