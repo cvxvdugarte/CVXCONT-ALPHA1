ed2965af54850076aca07e2d49e80ba1c3601d90
cvxcont <cvxvdugarte@users.noreply.github.com>
2016-06-10 09:43:54 -0400

    Create cia.php

---
+++ cia.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+<title>CVXCONT</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="chrome=1">
+    <link rel="stylesheet" href="stylesheets/github-dark.css">
+    <link href="bootstrap.css" rel="stylesheet">
+    <link href="bootstrap-theme.css" rel="stylesheet">
+    <script src="bootstrap.js"></script>
+    <script src="javascripts/respond.js"></script>
+    <script src="jquery-1.12.4.min.js"></script>
+    <link href="estilosvdugarte.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.5/angular.min.js"></script>
+</head>
+  <body class="bodyblack">
+  <?php
+$mysqli = new mysqli("mysql.hostinger.es","u754135709_vddb","*#L4S3PT1M4D3LM4DR1D","u754135709_dbvd");
+if($mysqli->connect_errno) {mysqli_autocommit($mysqli,TRUE);
+                     die("Connection failed: " . $mysqli->connect_error); }
+
+?> 
+</body>
+</html>
