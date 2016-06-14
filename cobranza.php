4a7aa1ef5fb22d95658428298d75640e146af4d1
cvxcont <cvxvdugarte@users.noreply.github.com>
2016-06-14 15:52:09 -0400

    Create cobranza.php

---
+++ cobranza.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<title>CVXCONT</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="chrome=1">
+    <link rel="stylesheet" href="stylesheets/github-dark.css">
+    <link href="bootstrap.css" rel="stylesheet">
+    <link href="bootstrap-theme.css" rel="stylesheet">
+    <script src="bootstrap.js"></script>
+    <script src="javascripts/respond.js"></script>
+    <script src="jquery-1.12.4.min.js"></script>
+    <link href="estilosvdugarte.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.5/angular.min.js"></script>-->
+</head>
+  <body class="bodyblack">
+  cobranza.php
+  </body>
+  </html>
