0fa37bf017931095ad847a97264cf2802cf6cc87
cvxcont <cvxvdugarte@users.noreply.github.com>
2016-06-14 09:33:59 -0400

    Update menuprincipal.php

--- menuprincipal.php
+++ menuprincipal.php
@@ -17,31 +17,30 @@
   <body class="bodyblack">
     <?
     session_start();
-    
     echo $_SESSION["USER"];
     session_destroy(); 
     ?>
 <nav class="navbar navbar-default navbar-fixed-top">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">MENUPRINCIPAL</a>
+      <a class="navbar-brand" href="menuprincipal.php">MENUPRINCIPAL</a>
     </div>
     <ul class="nav navbar-nav">
-      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">PERSONAS <span class="caret"></span></a>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="persona.php">PERSONAS <span class="caret"></span></a>
         <ul class="dropdown-menu">
           <li><a href="#">REGISTRO DE PERSONAS</a></li>
           <li><a href="#">REGISTRO DE USUARIOS</a></li>
          <li><a href="#">REGISTRO DE PROVEEDORES</a></li>
         </ul>
       </li>
-  <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">CXC <span class="caret"></span></a>
+  <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="cxc.php">CXC <span class="caret"></span></a>
         <ul class="dropdown-menu">
           <li><a href="cxc.php">REGISTRO DE FACTURAS</a></li>
           <li><a href="#">REGISTRO DE ANTICIPOS</a></li>
           <li><a href="#">REGISTRO DE CRUCES</a></li>
         </ul>
 </li>
-      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">CXP <span class="caret"></span></a>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="cxp.php">CXP <span class="caret"></span></a>
         <ul class="dropdown-menu">
           <li><a href="#">REGISTRO DE FACTURAS</a></li>
           <li><a href="#">REGISTRO DE ANTICIPOS</a></li>
@@ -49,15 +48,7 @@
           <li><a href="#">REPORTES</a></li>
         </ul>
         </li>
-     <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">CXC <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="#">REGISTRO DE FACTURAS</a></li>
-          <li><a href="#">REGISTRO DE ANTICIPOS</a></li>
-          <li><a href="#">REGISTRO DE CRUCES</a></li>
-        </ul>
-</li>
-
-   <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">TESORERIA <span class="caret"></span></a>
+   <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="tesoreria.php">TESORERIA <span class="caret"></span></a>
         <ul class="dropdown-menu">
           <li><a href="#">COBRANZA</a></li>
           <li><a href="#">BANCOS</a></li>
