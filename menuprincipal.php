19dafcbcef0e6908778e5c7b9a3490249cb43211
cvxcont <cvxvdugarte@users.noreply.github.com>
2016-06-14 13:30:13 -0400

    Update menuprincipal.php

--- menuprincipal.php
+++ menuprincipal.php
@@ -26,33 +26,46 @@
       <a class="navbar-brand" href="menuprincipal.php">MENUPRINCIPAL</a>
     </div>
     <ul class="nav navbar-nav">
-      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="persona.php">PERSONAS <span class="caret"></span></a>
+  <!--PERSONAS-->    
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown">PERSONAS <span class="caret"></span></a>
         <ul class="dropdown-menu">
-          <li><a href="#">REGISTRO DE PERSONAS</a></li>
-          <li><a href="#">REGISTRO DE USUARIOS</a></li>
-         <li><a href="#">REGISTRO DE PROVEEDORES</a></li>
+          <li><a href="cxcregpers.php">REGISTRO DE PERSONAS</a></li>
+          <li><a href="cxcregusr.php">REGISTRO DE USUARIOS</a></li>
+         <li><a href="cxcregprov.php">REGISTRO DE PROVEEDORES</a></li>
         </ul>
       </li>
-  <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="cxc.php">CXC <span class="caret"></span></a>
+  <!--CXC-->
+  <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown">CXC <span class="caret"></span></a>
+      <ul class="dropdown-menu">
+          <li><a href="cxcregfac.php">REGISTRO DE FACTURAS</a></li>
+          <li><a href="cxcregant.php">REGISTRO DE ANTICIPOS</a></li>
+          <li><a href="cxcregcruce.php">REGISTRO DE CRUCES</a></li>
+          <li><a href="cxcreport.php">REPORTES</a></li>
+      </ul>
+   </li>
+   <!--CXP-->
+   <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="cxp.php">CXP <span class="caret"></span></a>
         <ul class="dropdown-menu">
-          <li><a href="cxc.php">REGISTRO DE FACTURAS</a></li>
-          <li><a href="#">REGISTRO DE ANTICIPOS</a></li>
-          <li><a href="#">REGISTRO DE CRUCES</a></li>
+          <li><a href="cxpregfac.php">REGISTRO DE FACTURAS</a></li>
+          <li><a href="cxpregant.php">REGISTRO DE ANTICIPOS</a></li>
+          <li><a href="cxpregcruce.php">REGISTRO DE CRUCES</a></li>
+          <li><a href="cxpreport.php">REPORTES</a></li>
         </ul>
-</li>
-      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="cxp.php">CXP <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="#">REGISTRO DE FACTURAS</a></li>
-          <li><a href="#">REGISTRO DE ANTICIPOS</a></li>
-          <li><a href="#">REGISTRO DE CRUCES</a></li>
-          <li><a href="#">REPORTES</a></li>
-        </ul>
-        </li>
+   </li>
+   <!--TESORERIA-->
    <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="tesoreria.php">TESORERIA <span class="caret"></span></a>
         <ul class="dropdown-menu">
-          <li><a href="#">COBRANZA</a></li>
-          <li><a href="#">BANCOS</a></li>
-          <li><a href="#">REPORTES</a></li>
+          <li><a href="tesoro1.php">COBRANZA</a></li>
+          <li><a href="bancos.php">BANCOS</a></li>
+          <li><a href="tesororeport.php">REPORTES</a></li>
+        </ul>
+    </li>
+    <!--CONTABILIDAD-->
+    <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="contabilidad.php">CONTABILIDAD <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="contabilidad1.php">PLAN DE CU<a></li>
+          <li><a href="contabilidad2.php">BANCOS</a></li>
+          <li><a href="contabilidad3.php">REPORTES</a></li>
         </ul>
     </li>
     </ul>
