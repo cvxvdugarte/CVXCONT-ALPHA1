49f76a4ecc37b0488cb66f104bdc0b36c7f88649
cvxcont <cvxvdugarte@users.noreply.github.com>
2016-06-14 15:13:26 -0400

    Update menuprincipal.php

--- menuprincipal.php
+++ menuprincipal.php
@@ -21,7 +21,7 @@
     session_destroy(); 
     ?>
       <!--PERSONAS-->    
-    <div class="btn-group bodyblack">
+    <div class="btn-group-vertical btn-xs bodyblack">
       <button type="button" class="btn btn-primary"><a href="cxcregpers.php">REGISTRO DE PERSONAS</a></li>
       <button type="button" class="btn btn-primary"><a href="cxcregusr.php">REGISTRO DE USUARIOS</a></li>
       <button type="button" class="btn btn-primary"><a href="cxcregprov.php">REGISTRO DE PROVEEDORES</a></li>
