ef1bf127038d89e61f19e468d31950c4d0cf9084
cvxcont <cvxvdugarte@users.noreply.github.com>
2016-06-14 15:38:26 -0400

    Update menuprincipal.php

--- menuprincipal.php
+++ menuprincipal.php
@@ -4,7 +4,7 @@
 <title>CVXCONT</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="chrome=1">
-    <!--<link rel="stylesheet" href="stylesheets/github-dark.css">-->
+    <link rel="stylesheet" href="stylesheets/github-dark.css">
     <link href="bootstrap.css" rel="stylesheet">
     <link href="bootstrap-theme.css" rel="stylesheet">
     <script src="bootstrap.js"></script>
@@ -12,28 +12,23 @@
     <script src="jquery-1.12.4.min.js"></script>
     <link href="estilosvdugarte.css" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.5/angular.min.js"></script>
+    <!--<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.5/angular.min.js"></script>-->
 </head>
   <body class="bodyblack">
-    <?
-    session_start();
-    echo $_SESSION["USER"];
-    session_destroy(); 
-    ?>
       <!--PERSONAS-->    
     <div class="btn-group-vertical btn-group-xs">
-      <button type="button" class="btn btn-primary" role="button"><a href="cxcregpers.php">REGISTRO DE PERSONAS</a></li>
-      <button type="button" class="btn btn-primary" role="button"><a href="cxcregusr.php">REGISTRO DE USUARIOS</a></li>
-      <button type="button" class="btn btn-primary"><a href="cxcregprov.php">REGISTRO DE PROVEEDORES</a></li>
+      <button type="button" class="btn btn-block" role="button"><a href="cxcregpers.php">PERSONAS</a></li>
+      <button type="button" class="btn btn-primary" role="button"><a href="cxcregusr.php">USUARIOS</a></li>
+      <button type="button" class="btn btn-primary" role="button"><a href="cxcregprov.php">PROVEEDORES</a></li>
       <!--CXC-->
-         <button type="button" class="btn btn-primary" role="button"><a href="cxcregfac.php">REGISTRO DE FACTURAS</a></button>
-         <button type="button" class="btn btn-primary" role="button"><a href="cxcregant.php">REGISTRO DE ANTICIPOS</a></button>
-         <button type="button" class="btn btn-primary" role="button"><a href="cxcregcruce.php">REGISTRO DE CRUCES</a></button>
-         <button type="button" class="btn btn-primary"><a href="cxcreport.php">REPORTES</a></button>
+         <button type="button" class="btn btn-primary" role="button"><a href="cxcregfac.php">FACTURAS</a></button>
+         <button type="button" class="btn btn-primary" role="button"><a href="cxcregant.php">ANTICIPOS</a></button>
+         <button type="button" class="btn btn-primary" role="button"><a href="cxcregcruce.php">CRUCES</a></button>
+         <button type="button" class="btn btn-primary" role="button"><a href="cxcreport.php">REPORTES</a></button>
    <!--CXP-->
-         <button type="button" class="btn btn-primary" role="button"><a href="cxpregfac.php">REGISTRO DE FACTURAS</a></button>
-         <button type="button" class="btn btn-primary" role="button"><a href="cxpregant.php">REGISTRO DE ANTICIPOS</a></button>
-         <button type="button" class="btn btn-primary" role="button"><a href="cxpregcruce.php">REGISTRO DE CRUCES</a></button>
+         <button type="button" class="btn btn-primary" role="button"><a href="cxpregfac.php">FACTURAS</a></button>
+         <button type="button" class="btn btn-primary" role="button"><a href="cxpregant.php">ANTICIPOS</a></button>
+         <button type="button" class="btn btn-primary" role="button"><a href="cxpregcruce.php">CRUCES</a></button>
          <button type="button" class="btn btn-primary" role="button"><a href="cxpreport.php">REPORTES</a></button>
    <!--TESORERIA-->
          <button type="button" class="btn btn-primary" role="button"><a href="cobranza.php">COBRANZA</a></button>
